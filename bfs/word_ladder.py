1858d0fe6ef429dbd421f798ab74eab4238cb934
aduanfei123456 <564103067@qq.com>
2017-09-26 21:39:54 +0800

    standard solution

--- bfs/word_ladder.py
+++ bfs/word_ladder.py
@@ -17,7 +17,9 @@ Return 0 if there is no such transformation sequence.
 All words have the same length.
 All words contain only lowercase alphabetic characters.
 """
-import copy
+import logging
+log=logging.warning
+'''import copy
 def FindWord(wordList,beginWord,endWord):
     if(len(wordList)==0):
         return 0
@@ -63,9 +65,34 @@ def GetDiff(word,wordList):
             wl.append(w)
     #print(word,wl)
     return wl
+'''
+def ladderLength(beginWord,endWord,wordList):
+    beginSet=set()
+    endSet=set()
+    beginSet.add(beginWord)
+    endSet.add(endWord)
+    result=2
+    while len(beginSet)!=0 and len(endSet)!=0:
+        nextBegin=set()
+        for curWord in beginSet:
+            for rangeWord in wordRange(curWord):
+                if rangeWord in endSet:
+                    return result
+                if rangeWord in wordList:
+                    nextBegin.add(rangeWord)
+                    wordList.remove(rangeWord)
+        result+=1
+        print(nextBegin)
+        beginSet=nextBegin
+    return 0
+def wordRange(word):
+    for ind in range(len(word)):
+        tempC=word[ind]
+        for x in [chr(x) for x in range(ord('a'),ord('z')+1)]:
+            if x !=tempC:
+                yield word[:ind]+x+word[ind+1:]
 
 beginWord = "hit"
 endWord = "cog"
 wordList = ["hot","dot","dog","lot","log"]
-wordList.append(endWord)
-print(FindWord(wordList,beginWord,endWord))
+print(ladderLength(beginWord,endWord,wordList))
